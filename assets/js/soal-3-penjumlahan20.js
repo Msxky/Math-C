77ff57fada6517cf9fd73bd6785391bf40dcdfba
M Sukri <142651234+Msxky@users.noreply.github.com>
2023-10-27 20:40:19 +0700

    Add files via upload

---
+++ assets/js/soal-3-penjumlahan20.js
@@ -0,0 +1,105 @@
+const titleH = document.getElementById('h1Soal')
+const soal = document.getElementById('soal')
+const playSoalAudio = new Audio('/assets/audio/soal/penjumlahan20/soalKetiga.mp3')
+const option1A = new Audio('/assets/audio/soal/number/11.mp3');
+const option2A = new Audio('/assets/audio/soal/number/7.mp3');
+const option3A = new Audio('/assets/audio/soal/number/17.mp3');
+
+// OPTIONS
+const benar = new Audio('/assets/audio/soal/options/benar.mp3');
+const salah = new Audio('/assets/audio/soal/options/salah.mp3');
+const hore = new Audio('/assets/audio/soal/hore.mp3');
+
+playSoalAudio.play()
+function playSoal(){
+  playSoalAudio.play()
+}
+// OPTIONS 1
+function option1(){
+  option1A.play()
+  option2A.pause()
+  option3A.pause()
+  playSoalAudio.pause()
+  soal.textContent="13 + 4 = 11"
+  setTimeout(function() {
+    titleH.textContent="Loading.."
+    titleH.style.scale=".8"
+  }, 1000);
+  // output
+  setTimeout(function() {
+    salah.play()
+  }, 2000);
+  setTimeout(function() {
+  location.reload()
+  }, 5000);
+}
+// OPTIONS 2
+function option2(){
+  option1A.pause()
+  option2A.play()
+  option3A.pause()
+  playSoalAudio.pause()
+  soal.textContent="13 + 4 = 7"
+setTimeout(function() {
+  titleH.textContent="Loading.."
+  titleH.style.scale=".8"
+}, 1000);
+// output
+setTimeout(function() {
+  salah.play()
+}, 2000);
+setTimeout(function() {
+  location.reload()
+}, 5000);
+}
+// OPTIONS 3
+function option3(){
+  option1A.pause()
+  option2A.pause()
+  option3A.play()
+  playSoalAudio.pause()
+  soal.textContent="13 + 4 = 17"
+  setTimeout(function() {
+    titleH.textContent="Loading.."
+    titleH.style.scale=".8"
+  }, 1000);
+  // output
+  setTimeout(function() {
+    benar.play()
+  }, 2000);
+  setTimeout(function() {
+    hore.play()
+  }, 3000);
+  setTimeout(function() {
+  location.href="/assets/content/KamuPintar.html"
+  }, 8000);
+}
+
+
+// BUTTON FOOTER
+const kembali = new Audio("/assets/audio/kembali.mp3");
+function btnKembali() {
+  kembali.play();
+  lanjut.pause();
+  playSoalAudio.pause()
+  option1A.pause()
+  option2A.pause()
+  option3A.pause()
+  setTimeout(function() {
+    location.href="/assets/content/soal/soal-1-penjumlahan20.html";
+  }, 2000);
+}
+
+
+const lanjut = new Audio("/assets/audio/otomatis-lanjut.mp3");
+function btnLanjut() {
+  lanjut.play();
+  kembali.pause();
+  playSoalAudio.pause()
+  option1A.pause()
+  option2A.pause()
+  option3A.pause()
+setTimeout(function Penjumblahan() {
+    location.href="#";
+  }, 2000);
+}
---
+++ assets/js/title.js
@@ -0,0 +1,6 @@
+// CUSTOM
+document.title = "Math Calculations";
+// CUTOM
+
+let judul = document.getElementById("judul-apk");
+judul.textContent = (document.title);
