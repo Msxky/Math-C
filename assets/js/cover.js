e4d2f664e75449b76d1a738fdbe27e83cfd23f79
M Sukri <142651234+Msxky@users.noreply.github.com>
2023-10-29 19:28:11 +0700

    Update cover.js

--- assets/js/cover.js
+++ assets/js/cover.js
@@ -2,8 +2,15 @@ let coverBtn = document.getElementById("cover");
 const cover = new Audio("assets/audio/mulai.mp3");
 function Cover() {
   cover.play();
+  audio. pause();
   coverBtn.textContent = "loading..";
   setTimeout(function () {
     location.href = "/assets/content/pilih-lah.html";
   }, 2000);
+}
+
+const audio = new Audio("assets/audio/selamat-datang.mp3")
+function coverClick() {
+  audio.play();
+  cover.pause();
 }
