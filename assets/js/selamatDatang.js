ea81eac560245be17802190835e18eecfe2fb465
M Sukri <142651234+Msxky@users.noreply.github.com>
2023-10-29 08:58:13 +0700

    Delete assets directory

--- assets/js/selamatDatang.js
+++
@@ -1,4 +0,0 @@
-const selamatDatang = new Audio('/assets/audio/selamat-datang.mp3')
-setTimeout(function Penjumblahan() {
-  selamatDatang.play()
-}, 1000);
--- assets/js/title.js
+++
@@ -1,6 +0,0 @@
-// CUSTOM
-document.title = "Math Calculations";
-// CUTOM
-
-let judul = document.getElementById("judul-apk");
-judul.textContent = (document.title);
